94d6445be41ef806ab592ce690254cab5a80f552
Sala de Docentes - Facultad de Ingeniería - Bloque 4 <docentes.bog@ucc.edu.co>
2022-11-04 08:14:41 -0500

    con primera parte de ESTADOS

---
+++ front/src/actions/productActions.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+import { ALL_PRODUCTS_REQUEST,
+        ALL_PRODUCTS_SUCCES,
+        ALL_PRODUCTS_FAIL,
+        CLEAR_ERRORS} from '../constants/productConstants';
+
+
