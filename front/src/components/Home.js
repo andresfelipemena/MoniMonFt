aa5c3ad2a4394f6b079a303e3c8dff5614edbdf8
Sala de Docentes - Facultad de Ingeniería - Bloque 4 <docentes.bog@ucc.edu.co>
2022-11-05 11:00:31 -0500

    Home conectado a base de datos. Clase 20 oct - min: 59:05

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,9 +1,11 @@
 import React, { Fragment, useEffect } from 'react'
 import MetaData from './layout/MetaData';
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { getProducts } from '../actions/productActions';
 
 export const Home = () => {
+const {productos} = useSelector (state => state.products)
+
 const dispatch = useDispatch();
 useEffect (() => {
   dispatch(getProducts());
@@ -17,53 +19,22 @@ useEffect (() => {
         <section id="productos" className='container mt-1'>
           <div className='row'>
 
-            {/*Producto 1*/}
-            <div className='col-sm-4 col-md-6 col-lg-4 my-3'>
-              <div className='card p-3 rounded'>
-                <img className='card-img-top mx-auto' src="./images/physiotherapy.png" alt="Fisioterapia"></img>
-                <div className='card-body d-flex flex-column'>
-                  <h5 id="titulo_producto"> <a href='http://localhost:3000'>Fisioterapia</a> </h5>
-                  <div className='rating mt-auto'></div>
-                  <div className='rating-outer'>
-                    <div className='rating-inner'></div>
+            {productos && productos.map (producto =>(
+              <div key={producto._id} className='col-sm-4 col-md-6 col-lg-4 my-3'>
+                <div className='card p-3 rounded'>
+                  <img className='card-img-top mx-auto' src={producto.imagen[0].url} alt={producto.imagen[0].public_id}></img>
+                  <div className='card-body d-flex flex-column'>
+                    <h5 id="titulo_producto"> <a href='http://localhost:3000'>{producto.nombre}</a> </h5>
+                    <div className='rating mt-auto'></div>
+                    <div className='rating-outer'>
+                      <div className='rating-inner' style={{width: `${(producto.calificacion/5)*100}%`}}></div>
+                    </div>
+                    <span id="no_opiniones">{producto.numCalificaciones} Comentarios</span>
                   </div>
-                  <span id="no_opiniones">5 reviews</span>
+                  <p className='card-text'>${producto.precio}</p> <a href="http://localhost:3000" id="view_btn" className='btn btn-block'>Ver productos</a>
                 </div>
-                <p className='card-text'>$80.000</p> <a href="http://localhost:3000" id="view_btn" className='btn btn-block'>Ver productos</a>
               </div>
-            </div>
-
-            {/*Producto 2*/}
-            <div className='col-sm-4 col-md-6 col-lg-4 my-3'>
-              <div className='card p-3 rounded'>
-                <img className='card-img-top mx-auto' src="./images/trainning.png" alt="Entremamiento"></img>
-                <div className='card-body d-flex flex-column'>
-                  <h5 id="titulo_producto"> <a href='http://localhost:3000'>Entrenamiento</a> </h5>
-                  <div className='rating mt-auto'></div>
-                  <div className='rating-outer'>
-                    <div className='rating-inner'></div>
-                  </div>
-                  <span id="no_opiniones">3 reviews</span>
-                </div>
-                <p className='card-text'>$100.000</p> <a href="http://localhost:3000" id="view_btn" className='btn btn-block'>Ver productos</a>
-              </div>
-            </div>
-
-            {/*Producto 3*/}
-            <div className='col-sm-4 col-md-6 col-lg-4 my-3'>
-              <div className='card p-3 rounded'>
-                <img className='card-img-top mx-auto' src="./images/wellness.png" alt="Bienestar"></img>
-                <div className='card-body d-flex flex-column'>
-                  <h5 id="titulo_producto"> <a href='http://localhost:3000'>Bienestar</a> </h5>
-                  <div className='rating mt-auto'></div>
-                  <div className='rating-outer'>
-                    <div className='rating-inner'></div>
-                  </div>
-                  <span id="no_opiniones">3 reviews</span>
-                </div>
-                <p className='card-text'>$100.000</p> <a href="http://localhost:3000" id="view_btn" className='btn btn-block'>Ver productos</a>
-              </div>
-            </div>
+            ))}
 
           </div>
         </section>
