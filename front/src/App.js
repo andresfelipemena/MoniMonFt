4495a58aee8e304ea0057f405978034d0f7bb791
Sala de Docentes - Facultad de Ingeniería - Bloque 4 <docentes.bog@ucc.edu.co>
2022-11-08 08:23:39 -0500

    PriductList y NewProduct listos - Clase 28 Oct

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,10 @@ import { Footer } from './components/layout/Footer';
 import { Home } from './components/Home';
 import { ProductDetails } from './components/products/ProductDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Dashboard } from './components/admin/Dashboard';
+import ProductsList from './components/admin/ProductsList';
+import NewProduct from './components/admin/NewProduct';
+
 
 
 
@@ -19,6 +23,10 @@ function App() {
           <Route path='/Home' element={<Home />}/>
           <Route path='/' element={<Home />}/>
           <Route path='/producto/:id' element={<ProductDetails />}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+          <Route path="/productList" element={<ProductsList />}/>
+          <Route path="/nuevoProducto" element={<NewProduct />}/>
+          
         </Routes>
       </div>
       <Footer/>
--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react'
+import "../../App.css"
+import {Link} from "react-router-dom"
 
 const Header = () => {
   return (
@@ -11,29 +13,36 @@ const Header = () => {
             </div>
 
             <div className='col-12 col-md-6 mt-2 mt-md-0'>
-                    <div className="input-group">
-                        <input
-                            type="text"
-                            id="search_field"
-                            class="form-control"
-                            placeholder='¿Qué producto busca?'></input>
+                 <div className="input-group">
+                     <input
+                        type="text"
+                        id="search_field"
+                        class="form-control"
+                        placeholder='¿Qué producto busca?'></input>
                         <div class="input-group-append">
-                            <button id="search-btn" class="btn">
-                                <i class="fa fa-search-plus fa-2x text-white" aria-hidden="true"></i>
-                            </button>
+                         <button id="search-btn" class="btn">
+                            <i class="fa fa-search-plus fa-2x text-white" aria-hidden="true"></i>
+                         </button>
                         </div>
                     </div>
-                </div>
+             </div>
 
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                    <span><button className='btn' id="login_btn">Iniciar sesión</button></span>
-                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                  <div className="ml-4 dropdown d-inline">
+                      <Link to="#!" className="btn dropdown-toggle text-white mr-4" type="button"
+                          id="dropDownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <span>Panel de Control</span></Link>
+                      <div className='dropdown-menu' aria-labelledby='dropDownMenu'>
+                          <Link className="dropdown-item" to="/dashboard">Adm. Productos</Link>
+                          <Link className="dropdown-item" to="/">Pedidos</Link>
+                          <Link className="dropdown-item" to="/">Mi cuenta</Link>
+                          <Link className="dropdown-item" to="/">Cerrar Sesion</Link>
+                      </div>
+                  </div>
                     <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="false"></i>
                     <span className="ml-1" id="cart_count">2</span>
                 </div>
         </nav>
-
-
     </Fragment>
   )
 }
